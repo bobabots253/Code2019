a33a165abd16bdb0b947d1c309a098a66d148b79
Aaron Y <aaronyu330@gmail.com>
2019-02-06 15:41:18 -0800

    Pushing changes, reimplemented vision + characterization Robots

--- src/main/java/frc/robot/Hatch/HatchSubsystem.java
+++ src/main/java/frc/robot/Hatch/HatchSubsystem.java
@@ -1,24 +1,27 @@
 package frc.robot.Hatch;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class HatchSubsystem extends Subsystem {
 
+    public DoubleSolenoid eject = new DoubleSolenoid(1, 2, 3);
+
     private static HatchSubsystem instance = null;
+
     public static HatchSubsystem getInstance() {
         if (instance == null)
             instance = new HatchSubsystem();
         return instance;
     }
 
-    private HatchSubsystem(){
-
+    private HatchSubsystem() {
+        
     }
 
     @Override
     protected void initDefaultCommand() {
 
     }
-
 
 }
