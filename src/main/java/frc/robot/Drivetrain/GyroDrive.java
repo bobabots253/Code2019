a3a9470cb1af78ec437ea771e476146dd40322db
Aaron Y <aaronyu330@gmail.com>
2019-02-15 09:44:34 -0800

    I'm so sorry I just made the gyro loop a notifier xd

--- src/main/java/frc/robot/Drivetrain/GyroDrive.java
+++ src/main/java/frc/robot/Drivetrain/GyroDrive.java
@@ -1,54 +1,61 @@
 package frc.robot.Drivetrain;
 
+import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
-import frc.robot.Misc.GyroController;
 
-public class GyroDrive extends Command {
+public class GyroDrive extends Command implements Runnable{
 
-    private double kP = 0.0;
-    private double kI = 0;
-    private double kD = 0;
+    private double kP = 0.00;
+    private double kI = 0.00;
+    private double kD = 0.00;
 
     private double kF = 0.062; //Minimum motor output required to spin or move
+    private double last_error = 0;
+    private double sum_error = 0;
+
     private double last_theta = 0;
+
+    private double dt = 0.02;
+    private Notifier notifier = new Notifier(this);
     
-    //TODO: Remove this and make the command work based on the turn stick 
-    //AlSO TODO: Combine this and the other drive command, probably use an enum or something to manage which mode
-    private double target = 0; 
-    
-    //0.02 is robot loop speed (50Hz), consider placing into Notifier running something faster for better accuracy 
-    private GyroController gyro = new GyroController(kP, kI, kD, 0.02);
     
     public GyroDrive(){
         requires(Robot.drivetrain);
+        notifier.startPeriodic(dt);
     }
 
     protected void execute() {
-        double current_theta = Robot.oi.getLHPHeading(); //Getting the heading (left hand positive)
-        double omega = (current_theta - last_theta)/0.02; //Deriving smol omega, angular velocity
-        double ang_adjust = gyro.calculate(target-omega); //Calculating required correction values based on error
-        last_theta = current_theta;
+        /* Obtaining throttle and turn values */
+        double throttle = Robot.oi.getThrottleValue();
+        double turn = Robot.oi.getTurnValue();
 
-        SmartDashboard.putNumber("omega", omega);
+        double target = turn*20; //Rescaling turn stick to ang vel target
 
-        double left = -ang_adjust; 
-        double right = ang_adjust; 
-
-        SmartDashboard.putNumber("gyro response", left);
-
-        left = Robot.oi.getThrottleValue() + left + Robot.oi.getTurnValue();
-        right = Robot.oi.getThrottleValue() + right - Robot.oi.getTurnValue();
-    
-        //Makes sure kF is applied in the right direction
-        left += left > 0 ? kF : -kF; 
-        right += right > 0 ? kF : -kF;
-
-        SmartDashboard.putNumber("gyro left", left);
-        SmartDashboard.putNumber("gyro right", right);
+        /* Deriving angular velocity */
+        double theta = Robot.oi.getLHPHeading();
+        double w = (theta-last_theta)/dt;
+        last_theta = theta;
         
-        //Drives robot at calculated speeds (plus current throttle and turn values)
+        /* Performing PID math */
+        double error = target-w;
+        sum_error += error;
+        double w_adjust = kP * error + kI * sum_error + kD * (error-last_error)/dt;
+        last_error = error;
+
+        /* Adding minimum movement power */
+        double left = -w_adjust;
+        double right = w_adjust;
+
+        //Makes sure kF is applied in the right direction
+        left += left >= 0 ? kF : -kF; 
+        right += right >= 0 ? kF : -kF;
+
+        //Adding speed values
+        left += throttle;
+        right += throttle;
+
+        //Driving at calculated values
         Drivetrain.drive(left, right);
 
     }
@@ -61,5 +68,11 @@ public class GyroDrive extends Command {
     @Override
     protected void end(){
         Drivetrain.drive(0, 0);
+        notifier.stop();
+    }
+
+    @Override 
+    public void run(){
+        execute();
     }
 }
