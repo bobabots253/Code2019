e91f8517e0ffd54bce3c4db8414e1008f774685a
Aaron Y <aaronyu330@gmail.com>
2019-02-19 12:27:42 -0800

    Returning to old commit

--- src/main/java/frc/robot/Drivetrain/GyroDrive.java
+++ src/main/java/frc/robot/Drivetrain/GyroDrive.java
@@ -2,18 +2,71 @@ package frc.robot.Drivetrain;
 
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 
 public class GyroDrive extends Command {
 
-    // Creates a new Notifier to run this Command at nyoom speeds
+    private double kP = 0.003;
+    private double kI = 0.00;
+    private double kD = 0.0003;
+
+    private double kF = 0.06; //Minimum motor output required to spin or move
+    private double last_error = 0;
+    private double sum_error = 0;
+
+    private double last_theta = 0;
+
     private double dt = 0.02;
-    private Notifier notifier = new Notifier(new GyroPID(dt));
     
     
     public GyroDrive(){
         requires(Robot.drivetrain);
-        notifier.startPeriodic(dt); //Starting Notifier in constructor
+    }
+
+    protected void execute() {
+        /* Obtaining throttle and turn values */
+        double throttle = Robot.oi.getThrottleValue();
+        double turn = Robot.oi.getTurnValue();
+
+        double target = 0;
+        if(throttle >= 0){
+            target = turn*-400; //Rescaling turn stick to ang vel target
+        }else {
+            target = turn*400;
+        }
+        SmartDashboard.putNumber("Target", target);
+
+        /* Deriving angular velocity */
+        double theta = Robot.oi.getLHPHeading();
+        double w = (theta-last_theta)/dt;
+        SmartDashboard.putNumber("angvel", w);
+        last_theta = theta;
+        
+        /* Performing PID math */
+        double error = target-w;
+        SmartDashboard.putNumber("Error", error);
+        sum_error += error;
+        double w_adjust = kP * error + kI * sum_error + kD * (error-last_error)/dt;
+        SmartDashboard.putNumber("gyro response", w_adjust);
+        last_error = error;
+        System.out.println(w);
+
+        /* Adding minimum movement power */
+        double left = -w_adjust;
+        double right = w_adjust;
+
+        //Makes sure kF is applied in the right direction
+        left += left >= 0 ? kF : -kF; 
+        right += right >= 0 ? kF : -kF;
+
+        //Adding speed values
+        left += throttle;
+        right += throttle;
+
+        //Driving at calculated values
+        Drivetrain.drive(left, right);
+
     }
 
     @Override
@@ -24,7 +77,6 @@ public class GyroDrive extends Command {
     @Override
     protected void end(){
         Drivetrain.drive(0, 0);
-        notifier.stop();
     }
 
 }
